b0bd112d2838cfed9381808e7722d851dcc2b942
Tân Phạm <tanpham2421@gmail.com>
2026-02-02 09:25:33 +0700

    first commit

---
+++ cinema-management/src/test/java/Nhom5/cinema_management/CinemaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Nhom5.cinema_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
